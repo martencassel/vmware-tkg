900e9021e637907c2352c5737713ad1fdecba867
Marten Cassel <marten.cassel@gmail.com>
2024-04-10 23:05:57 +0200

    more

--- examples/upload-ova.ps1
+++ examples/upload-ova.ps1
@@ -1,34 +1,51 @@
-docker run -v $(pwd):/host -it vmware/powerclicore
-
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false
-
-cd /host
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false -Force -Confirm:$false 
 
 $Configuration = New-Object -TypeName PSObject -Property @{
     Server = "vcsa.lab.local"
-    VcenterUser = "Administrator@lab.local"
-    ResourcePool = "Resources"
-    Network = "LabNetwork"
-    OvaFilename = "photon-3-kube-v1.24.10+vmware.1-tkg.1-fbb49de6d1bf1f05a1c3711dea8b9330.ova"
-    OvaFilePath = "./photon-3-kube-v1.24.10+vmware.1-tkg.1-fbb49de6d1bf1f05a1c3711dea8b9330.ova"
-    Vmhost = "192.168.3.50"
-    Datastore = "datastore1"
-    FolderName = "TKG"
+    VCenterUser = "Administrator@lab.local"
+    ResourcePoolName = "Resources"
+    NetworkName = "LabNetwork"
+    VMHostName = "192.168.3.50"
+    DataStoreName = "datastore1"
 }
 
-$cred = Get-Credential -UserName $Configuration.VcenterUser -Message "Enter your password"
-Connect-VIServer -Server vcsa.lab.local -Credential $cred -Force
+Function UploadOVA {
+    param (
+        [string]$Name,
+        [string]$OVAFilename,
+        [string]$VMHostName,
+        [string]$DataStoreName,
+        [string]$FolderName,
+        [string]$Location,
+        [string]$NetworkName
+    )
+    $VMHost           = Get-VMHost -Name $VMHostName
+    $Location         = Get-ResourcePool -Name $Location
+    $DataStore        = Get-Datastore -Name $DataStoreName
+    $Folder           = Get-Folder -Name $FolderName
+    $OVAConfiguration = Get-OvfConfiguration -Ovf $OVAFilename
+    $OVAConfiguration.NetworkMapping.nic0.Value = $NetworkName
+    Import-VApp -Name $Name -Source $OVAFilename -VMHost $VMHost -Datastore $Datastore -InventoryLocation $Folder -Location $Location -OvfConfiguration $OVAConfiguration
+    Set-VM -VM $OVAFilename -ToTemplate -Confirm:$false
+}
+$Cred = Get-Credential -UserName $Configuration.VCenterUser
+Connect-VIServer -Server vcsa.lab.local -Credential $Cred -Force
 
-$vmhost = Get-VMHost -Name $Configuration.Vmhost
-$location = Get-ResourcePool -Name $Configuration.ResourcePool
-$ds = Get-Datastore -Name $Configuration.Datastore
-$folder = Get-Folder -Name  $Configuration.FolderName
-$ovaConfig = Get-OvfConfiguration -Ovf $Configuration.OvaFilePath
-$ovaConfig.NetworkMapping.nic0.Value = $Configuration.Network
+$TKGVersion = "2.1.1"
+$FolderName = "tkg-$TKGVersion"
+(Get-Datacenter)[0]|get-folder -Name vm | New-Folder $FolderName
 
-Import-VApp -Name $ova_filename -Source $ova -VMHost $vmhost -Datastore $ds -InventoryLocation $folder -Location $location -OvfConfiguration $ovaConfig
-Set-VM -VM $ova_filename -ToTemplate
+$PhotonImages = @(
+    "photon-3-kube-v1.24.10+vmware.1-tkg.1-fbb49de6d1bf1f05a1c3711dea8b9330.ova",
+    "photon-3-kube-v1.23.16+vmware.1-tkg.1-0baa1bef6f5d4a3ca3abe31fd4cd1607.ova",
+    "photon-3-kube-v1.22.17+vmware.1-tkg.1-a10ef8e088cc2c89418bca79a2fcc594.ova"
+)
+foreach($OVAFile in $PhotonImages) {
+    get-tkg-file.sh $TKGVersion $OVAFile $OVAFile
+}
+foreach ($OVAFile in $PhotonImages) {
+    UploadOVA -Name $OVAFile -OVAFilename $OVAFile -VMHostName $Configuration.VMHostName -DataStoreName $Configuration.DataStoreName -FolderName $FolderName -Location $Configuration.ResourcePoolName -NetworkName $Configuration.NetworkName
+}
 
-Get-Template
-
-dir vi:/Datacenter/vm/TKG|fl
+#Get-Template
+#dir vi:/Datacenter/vm/TKG|fl
